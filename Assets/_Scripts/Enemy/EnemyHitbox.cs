f9fc2b864eaa26833a3ecbaf88c23ef943ef046f
CaptainCat911 <dimonax33@gmail.com>
2022-11-18 20:55:45 +0300

    Squashed commit of the following:
    commit b93af01591f52abcbbe1d52be783cd9dcdccf92c
    Author: CaptainCat911 <dimonax33@gmail.com>
    Date:   Fri Nov 18 20:54:31 2022 +0300
    
        Сделал новых оружий
    
    commit 22d9270ef9e3c3b80dedee3899f12e91419d2858
    Author: CaptainCat911 <dimonax33@gmail.com>
    Date:   Thu Nov 17 20:50:03 2022 +0300
    
        Подправил врагов, добавил снайперку
    
    commit bf41d567d5a229febff980498c96c74a1a591fd9
    Author: CaptainCat911 <dimonax33@gmail.com>
    Date:   Thu Nov 17 13:54:02 2022 +0300
    
        Поделал врагов, подправил скрипты и коллайдер плеера

--- Assets/_Scripts/Enemy/EnemyHitbox.cs
+++ Assets/_Scripts/Enemy/EnemyHitbox.cs
@@ -3,51 +3,138 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHitbox : MonoBehaviour
-{
+{    
     Enemy enemy;
-    //public WeaponHolder weaponHolder;
-    public float attackRadius;                              // радиус атаки
-    
-    void Start()
+    EnemyHitBoxPivot pivot;
+
+    [Header("Параметры атаки")]
+    [HideInInspector] public float lastAttack;              // время последнего удара (для перезарядки удара)
+    public float hitBoxRadius;                              // радиус атаки                                                               
+    public float cooldown = 0.5f;                           // перезардяка атаки
+    public int damage = 1;                                  // урон
+    public float pushForce = 10;                            // сила толчка
+    // Если ренж
+    public bool isRange;                                    // ренж атака
+    public GameObject bulletPrefab;
+    public GameObject effectRangeAttack;                    // эффект ренж атаки
+    public float bulletSpeed = 10;                          // скорость снаряда
+    public float forceBackFire = 10;                        // отдача
+
+    // Для флипа оружия
+    bool needFlip;                          // нужен флип (для правильного отображения оружия)    
+    bool leftFlip;                          // оружие слева
+    bool rightFlip = true;                  // оружие справа
+
+
+
+    private void Awake()
     {
         enemy = GetComponentInParent<Enemy>();
+        pivot = GetComponentInParent<EnemyHitBoxPivot>();
+    }
+
+    void Start()
+    {
+
     }
 
     
     void Update()
     {
         //Debug.Log(enemy.lastAttack);
+
         // Атака
-        if (enemy.readyToAttack && Time.time - enemy.lastAttack > enemy.cooldown)           // если готовы атаковать и кд готово
+        if (enemy.readyToAttack && Time.time - lastAttack > cooldown)               // если готовы атаковать и кд готово
         {
-            //Debug.Log("Attack!");
-            enemy.lastAttack = Time.time;                                                   // присваиваем время атаки
+            lastAttack = Time.time;                                                 // присваиваем время атаки
+            enemy.animator.SetTrigger("Attack");                                    // начинаем анимацию атаки
+        }
 
-            Collider2D[] collidersHitbox = Physics2D.OverlapCircleAll(transform.position, attackRadius);  // создаем круг в позиции объекта с радиусом
-            foreach (Collider2D enObjectBox in collidersHitbox)
-            {
-                if (enObjectBox == null)
-                {
-                    continue;
-                }
+        // Флип оружия
+        if (Mathf.Abs(pivot.aimAngle) > 90 && rightFlip)
+        {
+            needFlip = true;
+            leftFlip = true;
+            rightFlip = false;
+        }
+        if (Mathf.Abs(pivot.aimAngle) <= 90 && leftFlip)
+        {
+            needFlip = true;
+            rightFlip = true;
+            leftFlip = false;
+        }
+        if (needFlip)
+        {
+            Flip();
+        }
+    }
 
-                if (enObjectBox.gameObject.TryGetComponent<Player>(out Player player))                  // ищем скрипт плеера
-                {
-                    player.TakeDamage(enemy.attackDamage);                                              // наносим урон
-                    Vector2 vec2 = (player.transform.position - transform.position).normalized;         // вычисляем вектор направления удара
-                    player.rb2D.AddForce(vec2 * enemy.pushForce, ForceMode2D.Impulse);                  // даём импульс
-                    enemy.animator.SetTrigger("Attack");                                                // начинаем анимацию
-                    //Debug.Log("Player!");
-                }
-                collidersHitbox = null;                                                                 // сбрасываем все найденные объекты (на самом деле непонятно как это работает)
-            }            
+    public void Attack()
+    {
+        if (!isRange)
+        {
+            MeleeAttack();                                                      // мили атака
+        }
+        else if (isRange)
+        {
+            RangeAttack();                                                      // ренж атака
         }        
+    }
+
+    void MeleeAttack()
+    {
+        Collider2D[] collidersHitbox = Physics2D.OverlapCircleAll(transform.position, hitBoxRadius);    // создаем круг в позиции объекта с радиусом
+        foreach (Collider2D enObjectBox in collidersHitbox)
+        {
+            if (enObjectBox == null)
+            {
+                continue;
+            }
+
+            if (enObjectBox.gameObject.TryGetComponent<Player>(out Player player))                  // ищем скрипт плеера
+            {
+                player.TakeDamage(damage);                                                          // наносим урон
+                Vector2 vec2 = (player.transform.position - transform.position).normalized;         // вычисляем вектор направления удара
+                player.rb2D.AddForce(vec2 * pushForce, ForceMode2D.Impulse);                        // даём импульс                                                                                                                  
+            }
+            collidersHitbox = null;                                                                 // сбрасываем все найденные объекты (на самом деле непонятно как это работает)
+        }
+    }
+
+
+    public void EffectRangeAttack()
+    {
+        if (effectRangeAttack)
+            Instantiate(effectRangeAttack, pivot.transform.position + new Vector3(0, 0.1f, 0), Quaternion.identity);                  // создаем эффект выстрела (если он есть)
+    }
+
+    void RangeAttack()
+    {        
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);              // создаем префаб снаряда с позицией и поворотом якоря
+        bullet.GetComponent<Bullet>().damage = damage;                                                      // присваиваем урон снаряду
+        bullet.GetComponent<Bullet>().pushForce = pushForce;                                                // присваиваем урон снаряду
+        bullet.GetComponent<Rigidbody2D>().AddForce(transform.right * bulletSpeed, ForceMode2D.Impulse);    // даём импульс
+        //bullet.transform.Rotate(0.0f, 0.0f, -90.0f, Space.Self);                                          // поворачиваем снаряд
+        enemy.ForceBackFire(transform.position, forceBackFire);                                             // даём отдачу         
+    }
+
+    void Flip()
+    {
+        if (leftFlip)                                                                                   // разворот налево
+        {
+            transform.localScale = new Vector3(transform.localScale.x, -1, transform.localScale.z);     // поворачиваем оружие через scale
+        }
+        if (rightFlip)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, 1, transform.localScale.z);
+        }
+        needFlip = false;
     }
 
     void OnDrawGizmosSelected()
     {
         // Draw a yellow sphere at the transform's position
         Gizmos.color = Color.yellow;        
-        Gizmos.DrawWireSphere(transform.position, attackRadius);
+        Gizmos.DrawWireSphere(transform.position, hitBoxRadius);
     }
 }
